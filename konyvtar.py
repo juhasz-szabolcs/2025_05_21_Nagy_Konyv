ecdd05cb790a8e6e7ec9d35a8f6829515414597b
juhasz-szabolcs <juhasz.szabolcs@szbi-pg.hu>
2025-05-21 14:26:15 +0200

    3.2 feladat_konyvek szama

--- konyvtar.py
+++ konyvtar.py
@@ -13,11 +13,19 @@ class Konyvtar:
                 adatok = sor.strip().split(';')
                 nev = adatok[0]
                 szul_ev = int(adatok[1])
-                hal_ev = int(adatok[2]) 
+                hal_ev = int(adatok[2]) if adatok[2] else 2005
                 nemzetiseg = adatok[3]
                 cim = adatok[4]
                 helyezes = adatok[5]
 
                 self.konyvek.append(Konyv(nev, szul_ev, hal_ev, nemzetiseg, cim, helyezes))
+
+    def konyvek_db(self):
+        print(f"3.2 feladat. Az állományban {len(self.konyvek)} db könyv szerepel")
     
-    
+konyvtar = Konyvtar()
+# print(konyvtar.konyvek)
+# for konyv in konyvtar.konyvek:
+#     print(konyv.nev, konyv.cim)
+# konyvtar.fajl_beolvasas
+konyvtar.konyvek_db()
