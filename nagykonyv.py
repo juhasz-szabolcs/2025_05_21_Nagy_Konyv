6f6eef8c20d22ebe3c995ac7535bc4d4178b8de9
juhasz-szabolcs <juhasz.szabolcs@szbi-pg.hu>
2025-05-21 13:21:46 +0200

    Konyv osztaly létrehozása

---
+++ nagykonyv.py
@@ -0,0 +1,8 @@
+class Konyv:
+    def __init__(self, nev, szul_ev, hal_ev, nemzetiseg, cim, helyezes):
+        self.nev = nev
+        self.szul_ev = szul_ev
+        self.hal_ev = hal_ev
+        self.nemzetiseg = nemzetiseg
+        self.cim = cim
+        self.helyezes = helyezes
